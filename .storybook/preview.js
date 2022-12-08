88ce036e2ffb7e54b20dc9ea6e9d73193a207ec0
Wildan Zulfikar <hey@wzulfikar.com>
2022-12-08 18:54:51 +0200

    Handle `warning` event before installing twind

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,11 +2,6 @@ import "@storybook/addon-actions/register";
 import { addDecorator } from "@storybook/react";
 import { withConsole } from "@storybook/addon-console";
 
-// Install twind
-import { install, defineConfig } from "@twind/core";
-import presetTailwind from "@twind/preset-tailwind";
-install(defineConfig({ presets: [presetTailwind()] }));
-
 // This will silent the warning because we passed the `cancelable: true`
 window.addEventListener("warning", (event) => {
   // prevent default console.warn(`[<code>] <message>: <detail>`) logging
@@ -18,5 +13,10 @@ window.addEventListener("warning", (event) => {
   // { message: '...', code: 'TWIND_INVALID_CSS', detail: '<css>'}
   console.warn("actual warning:", warning);
 });
+
+// Install twind
+import { install, defineConfig } from "@twind/core";
+import presetTailwind from "@twind/preset-tailwind";
+install(defineConfig({ presets: [presetTailwind()] }));
 
 addDecorator((storyFn, context) => withConsole()(storyFn)(context));
