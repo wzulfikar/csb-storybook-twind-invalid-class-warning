7f3792e9d4a79df3a0a5b9b1ad99ac5b8ddc0936
Wildan Zulfikar <hey@wzulfikar.com>
2022-12-08 18:23:39 +0200

    f

--- src/Button.stories.jsx
+++ src/Button.stories.jsx
@@ -1,3 +1,9 @@
-export const Basic = () => {
+const Button = () => {
   return <button className="font-bold">My Button</button>;
 };
+
+export default {
+  component: Button
+};
+
+export const Basic = () => <Button />;
